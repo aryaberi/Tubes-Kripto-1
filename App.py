572058e89fcdddb330b80ecd1daaf983c1fd469c
arya beri rasidi <aryarasidi66@gmail.com>
2021-10-01 14:31:41 +0700

    add gui

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from Steganography import make_New_Image,decode_Image
+from Steganography import encrib, make_New_Image,decode_Image
 from tkinter import *
 from tkinter import filedialog
 import cv2
@@ -8,10 +8,15 @@ main.geometry('500x800+100+200')
 plain = Label(main)
 Label1 = Label(main)
 fileLabel = Label(main)
+encrip = Label(main)
+Label2 = Label(main)
 
 m = IntVar()
 mrd1 = Radiobutton(main,text="Sequencial" ,variable=m, value = 1)
 mrd2 = Radiobutton(main,text="Acak", variable=m, value = 2)
+mrd3 = Radiobutton(main,text="encript Sequencial" ,variable=m, value = 3)
+mrd4 = Radiobutton(main,text=" encript Acak", variable=m, value = 4)
+
 
 label = Label(main, text = "Masukan Text")
 entry = Entry(main,width=30)
@@ -53,7 +58,7 @@ openFile = Button(main, text="Pilih File", command=pilihFile)
 openFile2 = Button(main, text="Pilih File", command=pilihFile2)
 
 def encode(metode,image,filepath,message,key, nama):
-    global plain,Label1,chiper,Label2
+    global plain,Label1,encrip,Label2
     Value = make_New_Image(image,message,filepath,metode,nama,key)
     plain.pack_forget()
     Label1.pack_forget()
@@ -63,10 +68,20 @@ def encode(metode,image,filepath,message,key, nama):
     plain = Label(main, text = Value)
     plain.pack()
     plain.place(x=500,y=20)
+    if(metode == 3 or metode == 4):
+        text = encrib(message)
+        encrip.pack_forget()
+        Label2.pack_forget()
+        Label1 = Label(main, text = "Text telah di encrypht menjadi: ")
+        Label1.pack()
+        Label1.place(x=220,y=40)
+        plain = Label(main, text = text)
+        plain.pack()
+        plain.place(x=400,y=40)
+
 
 def decode(image,key):
         global plain,Label1
-        print("sedang decode")
         Text = decode_Image(image,key)
         plain.pack_forget()
         Label1.pack_forget()
@@ -87,35 +102,39 @@ mrd1.pack()
 mrd1.place(x=40,y=20)
 mrd2.pack()
 mrd2.place(x=140,y=20)
+mrd3.pack()
+mrd3.place(x=40,y=40)
+mrd4.pack()
+mrd4.place(x=160,y=40)
 
 
 
 label.pack()
-label.place(x=40,y=40)
+label.place(x=40,y=60)
 entry.pack(ipady=5)
-entry.place(x=40,y=70)
+entry.place(x=40,y=90)
 label2.pack()
-label2.place(x=40,y=90)
+label2.place(x=40,y=110)
 entry2.pack(ipady=5)
-entry2.place(x=40,y=120)
+entry2.place(x=40,y=140)
 label3.pack()
-label3.place(x=40,y=140)
+label3.place(x=40,y=160)
 entry3.pack(ipady=5)
-entry3.place(x=40,y=170)
+entry3.place(x=40,y=190)
 openFile.pack()
-openFile.place(x=40,y=190)
+openFile.place(x=40,y=210)
 button1.pack()
-button1.place(x=40,y=220)
+button1.place(x=40,y=240)
 judul2 = Label(main,text="Tampilkan Pesan")
 judul2.pack
-judul2.place(x=40,y=250)
+judul2.place(x=40,y=280)
 label4.pack()
-label4.place(x=40,y=270)
+label4.place(x=40,y=300)
 entry4.pack(ipady=5)
-entry4.place(x=40,y=300)
+entry4.place(x=40,y=320)
 openFile2.pack()
-openFile2.place(x=40,y=320)
+openFile2.place(x=40,y=340)
 button2.pack()
-button2.place(x=40,y=350)
+button2.place(x=40,y=370)
 
 main.mainloop()
