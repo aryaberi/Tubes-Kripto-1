59e04755646ca9714d3d1ee218cf1b8291101904
muhanggarisfanani7 <13518071@std.stei.itb.ac.id>
2021-10-01 18:39:29 +0700

    bugfix audiostegano isSeq value

--- audiostegano.py
+++ audiostegano.py
@@ -22,16 +22,18 @@ def insertMsg(msg, audioPath, isSeq):
     # The "secret" text message
     string = msg
     # If the length of message is bigger than the inserted audio, return error
-    if ((len(frame_bytes)-(len(string)*8*8))/8 < 0):
+    if (((len(frame_bytes)-(len(string)*8*8))/8)-1 < 0):
         print('Ukuran Audio tidak cukup menampung semua pesan, proses dibatalkan')
     else:
         # Append dummy data to fill out rest of the bytes. Receiver shall detect and remove these characters.
         string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
         # Convert text to bit array
         bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
+        bits = bits if isSeq else swapCouple(bits)
+        bits = [1]+bits if isSeq else [0]+bits
 
         # Replace LSB of each byte of the audio data by one bit from the text bit array
-        for i, bit in enumerate(bits if isSeq else swapCouple(bits)):
+        for i, bit in enumerate(bits):
             frame_bytes[i] = (frame_bytes[i] & 254) | bit
     
     # Get the modified bytes
@@ -43,13 +45,15 @@ def insertMsg(msg, audioPath, isSeq):
         fd.writeframes(frame_modified)
     song.close()
 
-def emitMsg(audioPath, isSeq):
+def emitMsg(audioPath):
     song = wave.open(audioPath, mode='rb')
     # Convert audio to byte array
     frame_bytes = bytearray(list(song.readframes(song.getnframes())))
 
     # Extract the LSB of each byte
     extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+    isSeq = extracted[0]
+    extracted = extracted[1:]
     extracted = extracted if isSeq else swapCouple(extracted)
     # Convert byte array back to string
     string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
@@ -60,5 +64,5 @@ def emitMsg(audioPath, isSeq):
     song.close()
     return decoded
 
-# insertMsg('Selamat Siang, perkenalkan nama saya Angga halo ASSALAMUALAIKUM?', "sample.wav", False)
-# print(emitMsg("audio_embedded.wav", False))
+# insertMsg('Selamat Siang, perkenalkan nama saya Angga halo ASSALAMUALAIKUM?', "sample.wav", True)
+# print(emitMsg("audio_embedded.wav"))
